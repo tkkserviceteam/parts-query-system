a732ce405905381b9d76506789a08215501c7565
tkkserviceteam <tkkserviceteam@gmail.com>
2026-05-11 00:09:37 +0800

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -5,6 +5,16 @@ const nextConfig = {
   compiler: {
     removeConsole: process.env.NODE_ENV === 'production',
   },
+async rewrites() {
+    return [
+      {
+        // 當使用者訪問 /tkkweb/... 時
+        source: '/tkkweb/:path*',
+        // 伺服器會在背後偷偷去抓這個 HTTP 的 IP 資料
+        destination: 'http://211.75.18.228/tkkweb/:path*'
+      }
+    ];
+  }
 };
 
 module.exports = nextConfig;
